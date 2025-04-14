5fcd9e11bade14152317aece0525da5705fd42ba
bipingup <biping9930@gmail.com>
2025-04-14 16:59:49 +0530

    This is my First Code checkin into github

---
+++ stud-per.py
@@ -0,0 +1,51 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+def load_model():
+    with open("student_lr_final_model.pkl","rb") as file:
+        model,scaler,le = pickle.load(file)
+    return model,scaler,le
+
+def preprocessing_input_data(data,scaler,le):
+    data['Extracurricular Activities']= le.transform([data['Extracurricular Activities']])[0]
+    df = pd.DataFrame([data])
+    df_tranformed = scaler.transform(df)
+    return df_tranformed
+
+def predict_data(data):
+    model,scaler,le = load_model()
+    processed_data = preprocessing_input_data(data,scaler,le)
+    prediction = model.predict(processed_data)
+    return prediction
+
+def main():
+    st.title("Student performance prediction")
+    st.write("Enter your data to get a prediction for your Performance")
+
+    hour_sutdied = st.number_input("Hour studied",min_value=1,max_value=10,value=5)
+    prvious_score = st.number_input("previous score",min_value=40, max_value=100,value=70)
+    extra = st.selectbox("extra curri activity",["Yes","No"])
+    sleeping_hour = st.number_input("Sleepin hours",min_value=4,max_value=10,value = 7)
+    number_of_peper_solved = st.number_input("number of question paper solved",min_value = 0, max_value = 10 , value = 5)
+
+    if st.button("predict-your_score"):
+        user_data = {
+            "Hours Studied":hour_sutdied,
+            "Previous Scores":prvious_score,
+            "Extracurricular Activities":extra,
+            "Sleep Hours":sleeping_hour,
+            "Sample Question Papers Practiced":number_of_peper_solved
+        }
+        prediction = predict_data(user_data)
+        st.success(f"your prediciotn result is {prediction}")
+        user_data['prediction'] = round(float(prediction[0]),2)
+        user_data = {key: int(value) if isinstance(value, np.integer) else float(value) if isinstance(value, np.floating) else value for key, value in user_data.items()}
+        # collection.insert_one(user_data)
+        
+if __name__ == "__main__":
+    main()
